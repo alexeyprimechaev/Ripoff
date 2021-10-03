710f58a54ef7b228a9b4063ffd9ac4dc963e7e23
alexeyprimechaev <51163708+alexeyprimechaev@users.noreply.github.com>
2021-10-03 23:31:28 +0300

    Initial Commit

---
+++ Ripoff/RipoffApp.swift
@@ -0,0 +1,17 @@
+//
+//  RipoffApp.swift
+//  Ripoff
+//
+//  Created by Alexey Primechaev on 10/3/21.
+//
+
+import SwiftUI
+
+@main
+struct RipoffApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
